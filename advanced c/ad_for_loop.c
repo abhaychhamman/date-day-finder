0b0c5cf553f9980aa96b8a2bdb2adff39ef6daf3
abhay singh <abhaysingh722@gmail.com>
2021-12-06 21:13:19 +0530

    added

---
+++ advanced c/ad_for_loop.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+ 
+void main(){
+
+
+int i=0;
+
+// // first type
+
+// for(; i ;){
+//  printf("this is abhay kumar singh");
+// }
+   
+
+
+
+// // second type
+
+// for(i=0; i<=5 ;printf("\n%d",i)){
+ 
+// i++;
+
+// }
+
+
+
+
+// third type
+
+
+i=1;
+int j=1;
+for( ; ; ){
+ 
+ 
+
+if(i>5){
+
+    break;
+}
+
+else{
+j+=i;
+    printf("%d\n",j);
+i+=j;
+
+}
+
+}
+
+
+// 2
+// 5
+// 13
+// 34
+
+
+
+}
