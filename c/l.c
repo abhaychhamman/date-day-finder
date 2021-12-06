b7c9bcd4e064e52cef56e6e00e5f4c397d7aa482
abhay singh <abhaysingh722@gmail.com>
2021-12-06 22:49:14 +0530

    added

---
+++ c/l.c
@@ -0,0 +1,28 @@
+// *
+// *
+// *
+// *
+// *
+// *
+// * * * * *
+
+#include <stdio.h>
+void main()
+{
+
+    for (int i = 0; i < 7; i++)
+    {
+
+        if (i == 6)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                printf("* ");
+            }
+        }
+
+        printf("*\n");
+    }
+}
