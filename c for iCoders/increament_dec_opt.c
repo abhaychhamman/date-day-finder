a0c347a4c484a330e8c119e82e8926d745272dc8
abhaychhamman <abhaysingh72680818@gmail.com>
2022-01-30 19:01:07 +0530

    added

---
+++ c for iCoders/increament_dec_opt.c
@@ -0,0 +1,19 @@
+// ***************increament and decreament operator *********************
+
+#include<stdio.h>
+void  main(){
+
+int a=5;
+
+  
+
+printf("%d\n",a);//5
+printf("%d\n",a++);//5
+printf("%d\n",++a);//7
+printf("%d\n",--a);//6
+printf("%d\n",a--);//6
+
+
+ 
+
+}
